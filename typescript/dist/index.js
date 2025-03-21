68f5d5ba7b29657d2456001ef622f2284af514ae
shaon477 <96894515+Shaon7@users.noreply.github.com>
2025-03-22 04:07:50 +0600

    Add files via upload

---
+++ typescript/dist/index.js
@@ -0,0 +1,6 @@
+console.log("Hello World");
+var mark = 88;
+if (mark == 88) {
+    mark += 2;
+}
+console.log(mark);
---
+++ typescript/src/index.ts
@@ -0,0 +1,9 @@
+console.log("Hello World");
+
+let mark:number = 88;
+
+if(mark==88)
+{
+    mark+=2;
+}
+console.log(mark);
